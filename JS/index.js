8b7ca90bdbef24246f1f5568b256abf2337a30f2
Morkie22 <108274154+Morkie22@users.noreply.github.com>
2022-10-16 22:28:23 -0500

    changes

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,29 @@
 function validFormFieldInputData() {
-
-const newTask = document.querySelector('#task');
-const task = newTaskInput.value;
-const newDescription = document.querySelector('#description');
-const description = newDescriptionInput.value;
-const selectStatusoftheproject = document.querySelector('#status of the project');
-const statusOfTheProject = newselectStatusOfTheProjectInput.value;
-const newDueDate = document.querySelector('#due date');
-const dueDate = newDueDateInput.value;
-const newAssignTo = document.querySelector('#assignTo');
-const asssignTo = newAssignToInput.value;
-const submit = document.querySelector('#submit');
+	const form = document.getElementById('name of the task');
+	const newTask = document.querySelector('#task');
+	const task = newTaskInput.value;
+	const newDescription = document.querySelector('#description');
+	const description = newDescriptionInput.value;
+	const selectStatusoftheproject = document.querySelector('#status of the project');
+	const statusOfTheProject = newselectStatusOfTheProjectInput.value;
+	const newDueDate = document.querySelector('#due date');
+	const dueDate = newDueDateInput.value;
+	const newAssignTo = document.querySelector('#assignTo');
+	const asssignTo = newAssignToInput.value;
 }
-form.addEventListener('submit', function (e) {
-  // prevent the form from submitting
-  e.preventDefault();
 
+function submittedForm() {
+	alert("The form was submitted");
+}
+const button = document.getElementById('button');
+button.addEventListener('click', submittedForm);
+
+const taskHtml = createTaskHtml();
+"./taskManager.js";
+const saveTask = document.querySelector(taskList)
+taskList.addEventListener("click", function() {
+	if(yes.clicked === done) {
+		done
+	}
 });
-//up to this point Input code done by Nataly/banchi/etiopia/please review//
+Node.parentTaskList(parentTask);
--- JS/taskManager.js
+++ JS/taskManager.js
@@ -1,62 +1,60 @@
-//please review
-function createTaskHTML (task, description,assignedTo, dueDate, status){
-const html = 
-<li class="list-group-item">
-<div class="d-flex w-100 mt-2 justify-content-between align-items-center">
-    <h5>${task}</h5>
-    <span class="badge badge-danger">${statusOfTheProject}</span>
-</div>
-<div class="d-flex w-100 mb-3 justify-content-between">
-    <small>Assigned To: ${assignedTo}</small>
-    <small>dueDate: ${21/11/2022}</small>
-</div>
-<p>${description}</p>
-</li>
-
-}
-//done by nataly start of task 6
-//Nataly/banchi/etiopiawork
-class TaskManager {
-    constructor (tasks){
-      this.task = "tasks";
-      const trash = {
-      id: trash,
-      name: 'Take out the trash',
-      description: 'Take out the trash to the rental home',
-      assignedTo: 'Ethiopia',
-      status: 'On progress'  
-}; 
-    const cook = {
-      id: cook,
-      name: 'Cook Dinner',
-      description: 'Prepare a healthy serving of salads for the family tonight',
-      assignedTo: '',
-      status: 'Completed'
-  }
-    const laundary = {
-      id: laundary, 
-      name: 'laundary',
-      description: 'do launday on saturday',
-      assignTo: 'Banchi',
-      status: 'on Progress',
-  }
-      
-    }
-  };
   class TaskManager {
-      constructor(currentId = 0) {
-        this.currentId = currentId;
-        this.tasks = [];    }
-    
-      addTask(task, description, assignedTo, dueDate, status) {
+    constructor(currentId = 0) {
+      this.currentId = currentId;
+      this.tasks = [];    
+    this.tasks.push('id:', 'name:', 'description: ', 'assignTo:', 'dueDate:', 'status:')
+    }  
+      getTaskById(){
+        for (let index = 0; index < this.task.length; index++) {
+	let tasks = array[index];
+	tasks
+}; newMethod().tasks = [];
+        let arrayLength = this.tasks.length;
+        for (let i = 0; i < this.tasks.Length; i++) {
+            console.log(this.tasks[i]);
+        }
+      }
+    newMethod() {
+      return this;
+    }
+
+      addTask(name, description, assignedTo, dueDate, status) {
         this.tasks.push({
-          Id: this.currentId++,
-          tasks: task,
+          currentId: this.currentId,
+          name: name,
           description: description,
           assignedTo: assignedTo,
           dueDate: dueDate,
-          status: 'on Progress',
+          status: "TODO",
         });
-      
+        this.currentId++;
       }
   }
+function createTaskHtml (taskname, taskid, taskdescription, taskassignedTo, taskdueDate, taskstatus) {
+  <button type = "button" class = "done-button" > Mark as Done </button>
+     const html = `
+          <li class="list-group-item">
+              <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+                  <h5>Please add a task name :${name}</h5>
+                  <span class="badge badge-danger">TODO</span>
+              </div>
+              <div class="d-flex w-100 mb-3 justify-content-between">
+                  <small>Assigned To: ${assignedTo}</small>
+                  <small>Due date: ${dueDate}</small>
+                  <small>Status of the task: ${status}</small>
+                  <small>Id:${id}</small>
+              </div>
+              <p>${description}</p>
+          </li>
+      `}     
+    render() ;{
+      const tasksHtmlList = []
+       for (let i = 0; i < this.task.length; i++) {
+      const task = this.tasks[i]
+      const formattedDate = date.prototype.getdueDate
+      const taskHtml = createTaskHtml(task.id, task.name, task,description, task.assignedTo, formattedDate, task.status)
+      tasksHtmlList.push(taskHtml)
+} 
+}
+const tasksHtmlList = [document.getElementById("task").innerHTML, document.getElementById("description").innerHTML, document.getElementById("assignTo").innerHTML, document.getElementById("dueDate").innerHTML, "status"];
+let foundTask = taskid.array
